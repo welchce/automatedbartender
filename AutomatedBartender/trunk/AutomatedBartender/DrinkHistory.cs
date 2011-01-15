a37f97ec4535b99751f7fdaf361f3a507380ddbe
bnl1624 <bnl1624@gmail.com>
2011-01-15 23:04:51 +0000

    finished styling need to finish naming

--- AutomatedBartender/trunk/AutomatedBartender/DrinkHistory.cs
+++ AutomatedBartender/trunk/AutomatedBartender/DrinkHistory.cs
@@ -14,6 +14,7 @@ namespace AutomatedBartender
         public DrinkHistory()
         {
             InitializeComponent();
+            AutomatedBartender.WindowProperties.resizeScreen(this);
         }
     }
 }
