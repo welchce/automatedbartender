3607178e1d73919074c03adf7555c5ded350dc22
bnl1624@gmail.com <bnl1624@gmail.com@7a0c44f5-ddec-eee2-d26e-374765a5f38b>
2011-01-15 22:47:50 +0000

    added object to scree

--- trunk/AutomatedBartender/trunk/AutomatedBartender/AddUserScreen.Designer.cs
+++ trunk/AutomatedBartender/trunk/AutomatedBartender/AddUserScreen.Designer.cs
@@ -28,19 +28,84 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.AddUserYesbtm = new System.Windows.Forms.Button();
+            this.AddUserNobtn = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.tableLayoutPanel1.SuspendLayout();
             this.SuspendLayout();
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom)
+                        | System.Windows.Forms.AnchorStyles.Left)
+                        | System.Windows.Forms.AnchorStyles.Right)));
+            this.tableLayoutPanel1.ColumnCount = 1;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanel1.Controls.Add(this.AddUserYesbtm, 0, 1);
+            this.tableLayoutPanel1.Controls.Add(this.AddUserNobtn, 0, 2);
+            this.tableLayoutPanel1.Controls.Add(this.label1, 0, 0);
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(12, 12);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 3;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 33.33333F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 33.33333F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 33.33333F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(268, 247);
+            this.tableLayoutPanel1.TabIndex = 0;
+            // 
+            // AddUserYesbtm
+            // 
+            this.AddUserYesbtm.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.AddUserYesbtm.Location = new System.Drawing.Point(3, 85);
+            this.AddUserYesbtm.Name = "AddUserYesbtm";
+            this.AddUserYesbtm.Size = new System.Drawing.Size(262, 76);
+            this.AddUserYesbtm.TabIndex = 0;
+            this.AddUserYesbtm.Text = "Yes";
+            this.AddUserYesbtm.UseVisualStyleBackColor = true;
+            // 
+            // AddUserNobtn
+            // 
+            this.AddUserNobtn.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.AddUserNobtn.Location = new System.Drawing.Point(3, 167);
+            this.AddUserNobtn.Name = "AddUserNobtn";
+            this.AddUserNobtn.Size = new System.Drawing.Size(262, 77);
+            this.AddUserNobtn.TabIndex = 1;
+            this.AddUserNobtn.Text = "No";
+            this.AddUserNobtn.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            this.label1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom)));
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(9, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(250, 82);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Would you like to be added?";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
             // 
             // AddUserScreen
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(292, 271);
+            this.Controls.Add(this.tableLayoutPanel1);
             this.Name = "AddUserScreen";
-            this.Text = "Form";
+            this.Text = "Add User Screen";
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            this.tableLayoutPanel1.ResumeLayout(false);
+            this.tableLayoutPanel1.PerformLayout();
             this.ResumeLayout(false);
 
         }
 
         #endregion
+
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.Button AddUserYesbtm;
+        private System.Windows.Forms.Button AddUserNobtn;
+        private System.Windows.Forms.Label label1;
     }
 }
--- trunk/AutomatedBartender/trunk/AutomatedBartender/AddUserScreen.cs
+++ trunk/AutomatedBartender/trunk/AutomatedBartender/AddUserScreen.cs
@@ -14,6 +14,7 @@ namespace AutomatedBartender
         public AddUserScreen()
         {
             InitializeComponent();
+            AutomatedBartender.WindowProperties.resizeScreen(this);
         }
     }
 }
