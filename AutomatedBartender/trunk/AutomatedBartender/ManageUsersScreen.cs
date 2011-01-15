8efa116f842b051e323b1ee26fc0849f9546437b
bnl1624 <bnl1624@gmail.com>
2011-01-15 23:21:02 +0000

    added objects to screen

--- AutomatedBartender/trunk/AutomatedBartender/ManageUsersScreen.cs
+++ AutomatedBartender/trunk/AutomatedBartender/ManageUsersScreen.cs
@@ -14,6 +14,7 @@ namespace AutomatedBartender
         public ManageUsersScreen()
         {
             InitializeComponent();
+            AutomatedBartender.WindowProperties.resizeScreen(this);
         }
     }
 }
