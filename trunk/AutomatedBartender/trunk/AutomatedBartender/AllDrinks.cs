b6e46a84a8e3e5f117751a953fe0018129060620
bnl1624@gmail.com <bnl1624@gmail.com@7a0c44f5-ddec-eee2-d26e-374765a5f38b>
2011-01-19 20:29:34 +0000

    new window

---
+++ trunk/AutomatedBartender/trunk/AutomatedBartender/AllDrinks.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace AutomatedBartender
+{
+    public partial class AllDrinks : Form
+    {
+        bool isAdmin = false;
+        public AllDrinks(bool admin)
+        {
+            InitializeComponent();
+            AutomatedBartender.WindowProperties.resizeScreen(this);
+            setIsAdmin(admin);
+        }
+
+        private bool getIsAdmin()
+        {
+            return isAdmin;
+        }
+
+        private void setIsAdmin(bool value)
+        {
+            isAdmin = value;
+        }
+
+        private void AllDrinkBackBtn_Click(object sender, EventArgs e)
+        {
+            if (getIsAdmin() == true)
+            {
+                Form adminMainScreen = new AdminMain();
+                adminMainScreen.Show();
+                this.Close();
+            }
+            else
+            {
+                Form userMainScreen = new UserMain();
+                userMainScreen.Show();
+                this.Close();
+            }
+        }
+
+    }
+}
