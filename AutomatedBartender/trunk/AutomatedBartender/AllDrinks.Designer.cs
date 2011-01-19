913788baf6a0889339058d6f36128cc8e4717cc3
bnl1624 <bnl1624@gmail.com>
2011-01-19 20:48:31 +0000

    made back button work

--- AutomatedBartender/trunk/AutomatedBartender/AllDrinks.Designer.cs
+++ AutomatedBartender/trunk/AutomatedBartender/AllDrinks.Designer.cs
@@ -28,11 +28,123 @@
 		/// </summary>
 		private void InitializeComponent()
 		{
-			this.components = new System.ComponentModel.Container();
-			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-			this.Text = "AllDrinks";
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.AllDrinksDataGrid = new System.Windows.Forms.DataGridView();
+            this.label1 = new System.Windows.Forms.Label();
+            this.tableLayoutPanel2 = new System.Windows.Forms.TableLayoutPanel();
+            this.AllDrinksSubmitBtn = new System.Windows.Forms.Button();
+            this.AllDrinksBackBtn = new System.Windows.Forms.Button();
+            this.tableLayoutPanel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.AllDrinksDataGrid)).BeginInit();
+            this.tableLayoutPanel2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom)
+                        | System.Windows.Forms.AnchorStyles.Left)
+                        | System.Windows.Forms.AnchorStyles.Right)));
+            this.tableLayoutPanel1.ColumnCount = 1;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanel1.Controls.Add(this.AllDrinksDataGrid, 0, 1);
+            this.tableLayoutPanel1.Controls.Add(this.label1, 0, 0);
+            this.tableLayoutPanel1.Controls.Add(this.tableLayoutPanel2, 0, 2);
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(12, 12);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 3;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 15F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 70F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 15F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(276, 242);
+            this.tableLayoutPanel1.TabIndex = 3;
+            // 
+            // AllDrinksDataGrid
+            // 
+            this.AllDrinksDataGrid.AllowDrop = true;
+            this.AllDrinksDataGrid.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom)
+                        | System.Windows.Forms.AnchorStyles.Left)
+                        | System.Windows.Forms.AnchorStyles.Right)));
+            this.AllDrinksDataGrid.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.AllDrinksDataGrid.Location = new System.Drawing.Point(3, 39);
+            this.AllDrinksDataGrid.Name = "AllDrinksDataGrid";
+            this.AllDrinksDataGrid.Size = new System.Drawing.Size(270, 163);
+            this.AllDrinksDataGrid.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom)));
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(94, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(88, 36);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "All Drinks";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // tableLayoutPanel2
+            // 
+            this.tableLayoutPanel2.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom)
+                        | System.Windows.Forms.AnchorStyles.Left)
+                        | System.Windows.Forms.AnchorStyles.Right)));
+            this.tableLayoutPanel2.ColumnCount = 4;
+            this.tableLayoutPanel2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 25F));
+            this.tableLayoutPanel2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 25F));
+            this.tableLayoutPanel2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 25F));
+            this.tableLayoutPanel2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 25F));
+            this.tableLayoutPanel2.Controls.Add(this.AllDrinksSubmitBtn, 2, 0);
+            this.tableLayoutPanel2.Controls.Add(this.AllDrinksBackBtn, 3, 0);
+            this.tableLayoutPanel2.Location = new System.Drawing.Point(3, 208);
+            this.tableLayoutPanel2.Name = "tableLayoutPanel2";
+            this.tableLayoutPanel2.RowCount = 1;
+            this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanel2.Size = new System.Drawing.Size(270, 31);
+            this.tableLayoutPanel2.TabIndex = 2;
+            // 
+            // AllDrinksSubmitBtn
+            // 
+            this.AllDrinksSubmitBtn.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.AllDrinksSubmitBtn.Location = new System.Drawing.Point(137, 3);
+            this.AllDrinksSubmitBtn.Name = "AllDrinksSubmitBtn";
+            this.AllDrinksSubmitBtn.Size = new System.Drawing.Size(61, 25);
+            this.AllDrinksSubmitBtn.TabIndex = 0;
+            this.AllDrinksSubmitBtn.Text = "Submit";
+            this.AllDrinksSubmitBtn.UseVisualStyleBackColor = true;
+            // 
+            // AllDrinksBackBtn
+            // 
+            this.AllDrinksBackBtn.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.AllDrinksBackBtn.Location = new System.Drawing.Point(204, 3);
+            this.AllDrinksBackBtn.Name = "AllDrinksBackBtn";
+            this.AllDrinksBackBtn.Size = new System.Drawing.Size(63, 25);
+            this.AllDrinksBackBtn.TabIndex = 1;
+            this.AllDrinksBackBtn.Text = "Back";
+            this.AllDrinksBackBtn.UseVisualStyleBackColor = true;
+            this.AllDrinksBackBtn.Click += new System.EventHandler(this.AllDrinksBackBtn_Click);
+            // 
+            // AllDrinks
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(300, 266);
+            this.Controls.Add(this.tableLayoutPanel1);
+            this.Name = "AllDrinks";
+            this.Text = "AllDrinks";
+            this.tableLayoutPanel1.ResumeLayout(false);
+            this.tableLayoutPanel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.AllDrinksDataGrid)).EndInit();
+            this.tableLayoutPanel2.ResumeLayout(false);
+            this.ResumeLayout(false);
+
 		}
 
 		#endregion
+
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.DataGridView AllDrinksDataGrid;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel2;
+        private System.Windows.Forms.Button AllDrinksSubmitBtn;
+        private System.Windows.Forms.Button AllDrinksBackBtn;
 	}
 }
