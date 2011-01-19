0f7a4b5c17dcc2fb7ea6b1ef03615ad9e5ec21ef
bnl1624 <bnl1624@gmail.com>
2011-01-19 20:40:36 +0000

    add all drinks

--- AutomatedBartender/trunk/AutomatedBartender/AllDrinks.cs
+++ AutomatedBartender/trunk/AutomatedBartender/AllDrinks.cs
@@ -11,39 +11,9 @@ namespace AutomatedBartender
 {
     public partial class AllDrinks : Form
     {
-        bool isAdmin = false;
-        public AllDrinks(bool admin)
+        public AllDrinks()
         {
             InitializeComponent();
-            AutomatedBartender.WindowProperties.resizeScreen(this);
-            setIsAdmin(admin);
         }
-
-        private bool getIsAdmin()
-        {
-            return isAdmin;
-        }
-
-        private void setIsAdmin(bool value)
-        {
-            isAdmin = value;
-        }
-
-        private void AllDrinkBackBtn_Click(object sender, EventArgs e)
-        {
-            if (getIsAdmin() == true)
-            {
-                Form adminMainScreen = new AdminMain();
-                adminMainScreen.Show();
-                this.Close();
-            }
-            else
-            {
-                Form userMainScreen = new UserMain();
-                userMainScreen.Show();
-                this.Close();
-            }
-        }
-
     }
 }
